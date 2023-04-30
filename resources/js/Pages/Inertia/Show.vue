679f52c658bbf3dc26b2a423aacf61790dc32a13
Rqixy <yuukisato1013@gmail.com>
2023-04-30 17:30:15 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia';
+import InertiaTest from '../InertiaTest.vue';
+
+defineProps({
+    id : String,
+    blog: Object
+})
+
+const deleteConfirm = id => {
+    // console.log(id);
+    Inertia.delete(`/inertia/${id})`, {
+        onBefore: () => confirm('本当に削除しますか？')
+    });
+}
+</script>
+
+<template>
+    {{ id }}<br>
+    {{ blog.title }}<br>
+    <button @click="deleteConfirm(blog.id)">削除</button><br>
+</template>
